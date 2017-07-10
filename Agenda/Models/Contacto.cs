36d40b01fddd5824b939dfbd6701edbfc7bf5b75
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-09 21:08:50 -0500

    Personalización de tablas.

--- Agenda/Models/Contacto.cs
+++ Agenda/Models/Contacto.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Agenda.Models
 {
+    [Table("Contactos")]
     public class Contacto
     {
         public int Id { get; set; }
+        [MaxLength(40)]
         public string Nombre { get; set; }
         public ICollection<Telefono> Telefonos { get; set; }
     }
--- Agenda/Models/Telefono.cs
+++ Agenda/Models/Telefono.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Agenda.Models
 {
+    [Table("Telefonos")]
     public class Telefono
     {
         public int Id { get; set; }
+        [MaxLength(16)]
         public string Numero { get; set; }
+        [MaxLength(40)]
         public string Nombre { get; set; }
 
         public Contacto Contacto { get; set; }
+        public int ContactoId { get; set; }
     }
 }
