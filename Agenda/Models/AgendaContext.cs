c09746593cb48bce39ab7bceb518ca6a090644e9
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-07-09 20:40:01 -0500

    Crear contexto.

---
+++ Agenda/Models/AgendaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(): base()
+        {
+
+        }
+
+        public DbSet<Contacto> Contactos { get; set; }
+        public DbSet<Telefono> Telefonos { get; set; }
+
+    }
+}
